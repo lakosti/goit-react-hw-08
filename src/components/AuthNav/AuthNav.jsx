a449d33d334ea148adda73099586bb534c4edfc3
Yaroslava <yaroslava.hovenko@gmail.com>
2024-05-22 23:09:58 +0300

    change name from register to login

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const AuthNav = () => {
   return (
     <div>
-      <NavLink to="/register">Register</NavLink>
+      <NavLink to="/register">Sign up</NavLink>
       <NavLink to="/login">Log in</NavLink>
     </div>
   );
