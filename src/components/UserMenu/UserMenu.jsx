04163474522d609fd8f57039e628c528ec6448a1
Yaroslava <yaroslava.hovenko@gmail.com>
2024-05-29 22:49:51 +0300

    finish with user menu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../../redux/auth/selectors";
 import { logOut } from "../../redux/auth/operations";
 import toast from "react-hot-toast";
+import css from "./UserMenu.module.css";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -16,9 +17,9 @@ const UserMenu = () => {
       });
   };
   return (
-    <div>
-      <p>Wellcome, {user.name}</p>
-      <button onClick={handleClick} type="button">
+    <div className={css.userMenu}>
+      <p className={css.userName}>Wellcome, {user.name}</p>
+      <button className={css.userBtn} onClick={handleClick} type="button">
         Sign out
       </button>
     </div>
